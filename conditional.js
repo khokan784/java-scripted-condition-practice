d6903bccbb9b8f9165b8229867b898764e6e4f22
khokan <khokancrmt@gmail.com>
2024-08-30 15:16:02 +0600

    added file

---
+++ conditional.js
@@ -0,0 +1,36 @@
+// var weight = 15;
+
+// if(weight < 20) {
+//     console.log("I will carry it by myself.");
+// }
+// else {
+//     console.log("I will rent a rickshaw")
+// }
+
+
+
+// and condition
+
+
+// const weight = 15;
+// const height = 5;
+
+// if(weight < 20 && height > 30) {
+//     console.log("I will carry it by myself.");
+// }
+// else {
+//     console.log("I will rent a rickshaw")
+// }
+
+
+// multi condition
+
+
+
+// const weight = 15;
+// const height = 50;
+// const hasCar = true;
+
+// if((weight < 10 || height > 30) && hasCar == true ) {
+//         console.log("I will carry it by myself.");
+//     }
---
+++ nested.js
@@ -0,0 +1,18 @@
+
+const money = 1;
+if(money > 300) {
+    console.log( "you are a rich man");
+}
+else {
+    if(money >100) {
+        console.log('you are a medium rich');
+    }
+    else {
+        if(money > 5) {
+            console.log( 'you are a poor man');
+        }
+        else{
+            console.log("ami kisu na")
+        }
+    }
+}
