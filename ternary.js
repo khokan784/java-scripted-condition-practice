c93e63a023cd6427635abe6d1c04fc1a5c5ef032
khokan <khokancrmt@gmail.com>
2024-08-30 20:26:51 +0600

    added ternary js file

---
+++ ternary.js
@@ -0,0 +1,35 @@
+// normal if else
+
+
+// const age = 10;
+// if(age >= 18) {
+//     console.log("you can provide vote");
+// }
+// else {
+//     console.log('You can,t provide vote');
+// }
+
+
+
+// simple ternary 
+
+
+// const age = 20;
+
+// age >= 18 ? console.log("You can provide vote") : console.log("You can,t provide vote")
+
+
+let price = 500;
+const isLeader = true;
+// if(isLeader === true) {
+//     price = 0;
+// }
+// else {
+//     price = price + 100;
+// }
+// console.log(price);
+
+
+// price = isLeader === true ? 0 : price + 100;
+
+let result = 10 < "5";
